0829b00a5a3e5101da41b6a984ff14145ac07603
Aniket Pradhan <aniket17133@iiitd.ac.in>
2019-08-05 23:18:48 +0530

    Added binary classification in GUI

--- scripts/gui.py
+++ scripts/gui.py
@@ -6,52 +6,77 @@ import subprocess
 class mywindow(QtWidgets.QMainWindow):
 
 	def gaussian_nb(self):
-		command = "python naive_bayes.py --train true --trainfile ../dataset/train2.tsv --nb_type GaussianNB"
+		if self.binary:
+			command = "python naive_bayes.py --train true --trainfile ../dataset/train2.tsv --nb_type GaussianNB --binary true"
+		else:
+			command = "python naive_bayes.py --train true --trainfile ../dataset/train2.tsv --nb_type GaussianNB"
 		command = command.strip().split()
 		test = subprocess.Popen(command, stdout=subprocess.PIPE)
 		output = test.communicate()[0]
 		self.ui.label.setText(output.decode("utf-8").strip())
 	
 	def bernoulli_nb(self):
-		command = "python naive_bayes.py --train true --trainfile ../dataset/train2.tsv --nb_type BernoulliNB"
+		if self.binary:
+			command = "python naive_bayes.py --train true --trainfile ../dataset/train2.tsv --nb_type BernoulliNB --binary true"
+		else:
+			command = "python naive_bayes.py --train true --trainfile ../dataset/train2.tsv --nb_type BernoulliNB"
 		command = command.strip().split()
 		test = subprocess.Popen(command, stdout=subprocess.PIPE)
 		output = test.communicate()[0]
 		self.ui.label.setText(output.decode("utf-8").strip())
 	
 	def mutinomial_nb(self):
-		command = "python naive_bayes.py --train true --trainfile ../dataset/train2.tsv --nb_type MultinomialNB"
+		if self.binary:
+			command = "python naive_bayes.py --train true --trainfile ../dataset/train2.tsv --nb_type MultinomialNB --binary true"
+		else:
+			command = "python naive_bayes.py --train true --trainfile ../dataset/train2.tsv --nb_type MultinomialNB"
 		command = command.strip().split()
 		test = subprocess.Popen(command, stdout=subprocess.PIPE)
 		output = test.communicate()[0]
 		self.ui.label.setText(output.decode("utf-8").strip())
 	
 	def nb_test(self):
-		command = "python naive_bayes.py --test true --testfile ../dataset/test2.tsv --model ../models/trained_data_nb.pickle"
+		if self.binary:
+			command = "python naive_bayes.py --test true --testfile ../dataset/test2.tsv --model ../models/trained_data_nb.pickle --binary true"
+		else:
+			command = "python naive_bayes.py --test true --testfile ../dataset/test2.tsv --model ../models/trained_data_nb.pickle"
 		command = command.strip().split()
 		test = subprocess.Popen(command, stdout=subprocess.PIPE)
 		output = test.communicate()[0]
 		self.ui.label.setText(output.decode("utf-8").strip())
 	
 	def logistic(self):
-		command = "python logistic.py --train true --trainfile ../dataset/train2.tsv"
+		if self.binary:
+			command = "python logistic.py --train true --trainfile ../dataset/train2.tsv --binary true"
+		else:
+			command = "python logistic.py --train true --trainfile ../dataset/train2.tsv"
 		command = command.strip().split()
 		test = subprocess.Popen(command, stdout=subprocess.PIPE)
 		output = test.communicate()[0]
 		self.ui.label.setText(output.decode("utf-8").strip())
 	
 	def logistic_test(self):
-		command = "python logistic.py --test true --testfile ../dataset/test2.tsv --model ../models/trained_data_logistic.pickle"
+		if self.binary:
+			command = "python logistic.py --test true --testfile ../dataset/test2.tsv --model ../models/trained_data_logistic.pickle --binary true"
+		else:
+			command = "python logistic.py --test true --testfile ../dataset/test2.tsv --model ../models/trained_data_logistic.pickle"
 		command = command.strip().split()
 		test = subprocess.Popen(command, stdout=subprocess.PIPE)
 		output = test.communicate()[0]
 		self.ui.label.setText(output.decode("utf-8").strip())
+	
+	def toggle_binary(self):
+		self.binary = not self.binary
 
 	def __init__(self):
 		super(mywindow, self).__init__()
 		self.ui = Ui_MainWindow()	
 		self.ui.setupUi(self)
 		self.ui.label.setFont(QtGui.QFont('SansSerif', 30))
+
+		self.binary = False
+
+		self.ui.checkBox.stateChanged.connect(self.toggle_binary)
 
 		self.ui.pushButton.clicked.connect(self.gaussian_nb)
 		self.ui.pushButton_2.clicked.connect(self.mutinomial_nb)
